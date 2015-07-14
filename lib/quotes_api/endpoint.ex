ff1f66bcb0fccf0ba10ac75b2ca81ef986a70a69
jaketrent <trent.jake@gmail.com>
2015-07-13 19:52:03 -0600

    Remove static assets and handlers

--- lib/quotes_api/endpoint.ex
+++ lib/quotes_api/endpoint.ex
@@ -1,13 +1,8 @@
 defmodule QuotesApi.Endpoint do
   use Phoenix.Endpoint, otp_app: :quotes_api
 
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phoenix.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/", from: :quotes_api, gzip: false,
-    only: ~w(css images js favicon.ico robots.txt)
+  plug Plug.RequestId
+  plug Plug.Logger
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
@@ -15,9 +10,6 @@ defmodule QuotesApi.Endpoint do
     plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
   end
-
-  plug Plug.RequestId
-  plug Plug.Logger
 
   plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
